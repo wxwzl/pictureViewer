06100527392fab01a7d2dcca290152306df5558d
wxwzl <zgkj456@163.com>
2021-09-19 19:17:33 +0800

    feat: 优化拖动、增加键盘支持、增加重置图片样式支持、增加图片预加载配置

--- packages/src/pictureViewer.vue
+++ packages/src/pictureViewer.vue
@@ -1,375 +1,386 @@
 <template>
-  <div class="pictureViewer" v-show="show">
-    <div class="mask"></div>
-    <div class="container">
-      <div ref="post" class="imagePost">
-        <!-- <img ref="img" :src="currentImgUrl" /> -->
-      </div>
+  <div class="pictureViewer" v-show="show" :style="{ 'z-index': zIndex }">
+    <div class="pictureViewer-mask"></div>
+    <div class="pictureViewer-container" ref="post">
+      <div class="pictureViewer-verticlehelp"></div>
     </div>
-    <slot name="before">
-      <button class="side left" @click="before">上一张</button>
-    </slot>
-    <slot name="next">
-      <button class="side right" @click="next">下一张</button>
-    </slot>
-    <slot name="close">
-      <button class="close" @click="close">X</button>
-    </slot>
-
-    <!-- <div class="side left" @click="before">
-      <div class="vertical-center"></div>
-      
-    </div> -->
-    <!-- <div class="side right" @click="next">
-      <div class="vertical-center"></div>
-      
-    </div> -->
-    <div class="bottom">
-      <slot name="bottom">
-        <button @click="magnify">+</button>
-        <button @click="deflate">-</button>
-        <button @click="handLeft">向左旋转</button>
-        <button @click="handRight">向右旋转</button>
+    <template v-if="$slots.actions">
+      <slot name="actions"></slot>
+    </template>
+    <template v-else>
+      <slot v-if="images.length > 1 && !isFirst" name="before">
+        <button class="pictureViewer-side left" @click="before">上一张</button>
       </slot>
-    </div>
+      <slot v-if="images.length > 1 && !isLast" name="next">
+        <button class="pictureViewer-side right" @click="next">下一张</button>
+      </slot>
+      <slot name="close">
+        <button class="pictureViewer-close" @click="close">X</button>
+      </slot>
+      <div class="pictureViewer-bottom">
+        <slot name="bottom">
+          <button @click="magnify">+</button>
+          <button @click="deflate">-</button>
+          <button @click="resetStyle">还原</button>
+          <button @click="handLeft">向左旋转</button>
+          <button @click="handRight">向右旋转</button>
+        </slot>
+      </div>
+    </template>
   </div>
 </template>
 <script>
-  export default {
-    name: "pictureViewer",
-    props: {
-      images: {
-        type: Array,
-        default: function () {
-          return [];
-        },
-      },
-      current: {
-        type: Number,
-        default: 0,
-      },
-      visible: {
-        type: Boolean,
-        default: false,
-      },
-      appendToBody: {
-        type: Boolean,
-        default: false,
+import { on, off, setStyle, rafThrottle, isFirefox } from "./utils";
+const mousewheelEventName = isFirefox() ? "DOMMouseScroll" : "mousewheel";
+const defaultTransform = {
+  scale: 0,
+  rotate: 0,
+  offsetX: 0,
+  offsetY: 0,
+  scale: 1,
+};
+export default {
+  name: "pictureViewer",
+  props: {
+    images: {
+      type: Array,
+      default: function () {
+        return [];
       },
     },
-    watch: {
-      images: {
-        handler() {
-          this.setImageUrl();
-        },
-        immediate: true,
-        deep: true,
+    zIndex: {
+      type: Number,
+      default: 1000,
+    },
+    current: {
+      type: Number,
+      default: 0,
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    appendToBody: {
+      type: Boolean,
+      default: false,
+    },
+    minRotate: {
+      type: Number,
+      default: 90,
+    },
+    preload: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  watch: {
+    images: {
+      handler() {
+        this.preLoadImages();
+        this.setImageUrl();
       },
-      current() {
-        if (this.current != this.currentIndex) {
-          this.currentIndex = this.current;
-          this.setImageUrl();
-        }
+      immediate: true,
+      deep: true,
+    },
+    visible: {
+      handler() {
+        this.installEvent();
+      },
+      immediate: true,
+    },
+    current() {
+      if (this.current != this.currentIndex) {
+        this.currentIndex = this.current;
+        this.setImageUrl();
+      }
+    },
+  },
+  data() {
+    return {
+      currentImgUrl: "",
+      currentIndex: 0,
+      imgNode: null,
+      transform: JSON.parse(JSON.stringify(defaultTransform)),
+    };
+  },
+  computed: {
+    show: {
+      get() {
+        return this.visible;
+      },
+      set(val) {
+        this.$emit("update:visible", val);
       },
     },
-    data() {
-      return {
-        currentImgUrl: "",
-        currentIndex: 0,
-        rotateIndex: 0,
-        imgNode: null,
-      };
+    isLast() {
+      return this.currentIndex == this.images.length - 1;
     },
-    computed: {
-      show: {
-        get() {
-          return this.visible;
-        },
-        set(val) {
-          this.$emit("update:visible", val);
-        },
-      },
+    isFirst() {
+      return this.currentIndex == 0;
     },
-    mounted() {
-      if (this.appendToBody) {
-        this.$el.parentNode.removeChild(this.$el);
-        document.body.append(this.$el);
+  },
+  mounted() {
+    if (this.appendToBody) {
+      this.$el.parentNode.removeChild(this.$el);
+      document.body.append(this.$el);
+    }
+    this.setImageUrl();
+  },
+  methods: {
+    installEvent() {
+      if (!this._eventInstalled && this.visible) {
+        this._eventInstalled = true;
+        this._keyDownHandler = (e) => {
+          e.stopPropagation();
+          const keyCode = e.keyCode;
+          switch (keyCode) {
+            // ESC
+            case 27:
+              this.close();
+              break;
+            // SPACE
+            case 32:
+              this.resetStyle();
+              break;
+            // LEFT_ARROW
+            case 37:
+              this.before();
+              break;
+            // UP_ARROW
+            case 38:
+              this.magnify();
+              break;
+            // RIGHT_ARROW
+            case 39:
+              this.next();
+              break;
+            // DOWN_ARROW
+            case 40:
+              this.deflate();
+              break;
+          }
+        };
+        this._mouseWheelHandler = rafThrottle((e) => {
+          const delta = e.wheelDelta ? e.wheelDelta : -e.detail;
+          if (delta > 0) {
+            this.magnify();
+          } else {
+            this.deflate();
+          }
+        });
+        on(document, "keydown", this._keyDownHandler);
+        on(document, mousewheelEventName, this._mouseWheelHandler);
+      }
+    },
+    uninstallEvent() {
+      off(document, "keydown", this._keyDownHandler);
+      off(document, mousewheelEventName, this._mouseWheelHandler);
+      this._keyDownHandler = null;
+      this._mouseWheelHandler = null;
+      this._eventInstalled = false;
+    },
+    handleMouseDown(e) {
+      const startX = e.pageX;
+      const startY = e.pageY;
+      const offsetX = this.transform.offsetX;
+      const offsetY = this.transform.offsetY;
+      this._dragHandler = rafThrottle((e) => {
+        this.transform.offsetX = offsetX + e.pageX - startX;
+        this.transform.offsetY = offsetY + e.pageY - startY;
+        this.setImgNodeStyle();
+      });
+      on(document, "mousemove", this._dragHandler);
+      on(document, "mouseup", (ev) => {
+        off(document, "mousemove", this._dragHandler);
+      });
+      e.preventDefault();
+    },
+    before() {
+      if (this.currentIndex <= 0) {
+        return;
+      }
+      if (this.currentIndex >= 1) {
+        this.currentIndex--;
+      } else {
+        this.currentIndex = 0;
       }
       this.setImageUrl();
-      this.bindEvent();
     },
-    methods: {
-      bindEvent() {
-        let postNode = this.$refs.post;
-        postNode.addEventListener(
-          "dragover",
-          (e) => {
-            e.preventDefault();
-          },
-          false
-        );
-        postNode.addEventListener(
-          "drop",
-          (e) => {
-            let data = e.dataTransfer.getData("Text");
-            try {
-              data = JSON.parse(data);
-            } catch (e) {
-              console.log(e);
-            }
-            this.setStyle(this.imgNode, {
-              top: e.offsetY - data.offsetY + "px",
-              left: e.offsetX - data.offsetX + "px",
-              "margin-top": 0,
-              "margin-left": 0,
-            });
-          },
-          false
-        );
-      },
-      before() {
-        if (this.currentIndex <= 0) {
-          return;
-        }
-        if (this.currentIndex >= 1) {
-          this.currentIndex--;
-        } else {
-          this.currentIndex = 0;
-        }
-        this.setImageUrl();
-      },
-      next() {
-        if (this.currentIndex >= this.images.length - 1) {
-          return;
-        }
-        if (this.currentIndex < this.images.length - 1) {
-          this.currentIndex++;
-        } else {
-          this.currentIndex = this.images.length - 1;
-        }
-        this.setImageUrl();
-      },
-      close() {
-        this.show = false;
-      },
-      emitIndexChange() {
-        if (this.currentIndex != this.current) {
-          this.$emit("update:current", this.currentIndex);
-        }
-      },
-      setImageUrl() {
-        if (!this.images[this.currentIndex]) {
-          return;
-        }
-        this.emitIndexChange();
+    next() {
+      if (this.currentIndex >= this.images.length - 1) {
+        return;
+      }
+      if (this.currentIndex < this.images.length - 1) {
+        this.currentIndex++;
+      } else {
+        this.currentIndex = this.images.length - 1;
+      }
+      this.setImageUrl();
+    },
+    close() {
+      this.show = false;
+      this.uninstallEvent();
+    },
+    emitIndexChange() {
+      if (this.currentIndex != this.current) {
+        this.$emit("update:current", this.currentIndex);
+      }
+    },
+    setImageUrl() {
+      if (!this.images[this.currentIndex]) {
+        return;
+      }
+      this.emitIndexChange();
+      let image = new Image();
+      image.src = this.images[this.currentIndex];
+      image.onload = this.handleImgLoaded.bind(this);
+      image.onerror = this.handleImgError.bind(this);
+    },
+    preLoadImages() {
+      let len = this.images.length;
+      let temp = 0;
+      function loadImg() {
         let image = new Image();
-        image.src = this.images[this.currentIndex];
-        image.onload = this.imgLoaded.bind(this);
-        image.ondragover = function (e) {
-          e.preventDefault();
-          e.stopPropagation();
-        };
-        image.ondragstart = function (e) {
-          // e.preventDefault();
-          let offsetX = e.offsetX;
-          let offsetY = e.offsetY;
-          e.dataTransfer.setData("Text", JSON.stringify({ offsetX: offsetX, offsetY: offsetY }));
-        };
-      },
-      imgLoaded(e) {
-        if (this.imgNode) {
-          this.imgNode.parentNode.removeChild(this.imgNode);
-        }
-        let imgNode = (this.imgNode = e.target);
-        let maxWidthStr = this.getStyle(this.$refs.post, "width");
-        let maxHeightStr = this.getStyle(this.$refs.post, "height");
-        let maxWidth = this.getNumberFromPx(maxWidthStr);
-        let maxHeight = this.getNumberFromPx(maxHeightStr);
-        let imgWidth = imgNode.width;
-        let imgHeight = imgNode.height;
-        let widthRadio = imgWidth / maxWidth;
-        let heightRadio = imgHeight / maxHeight;
-        let radio = 0.8;
-        let width = imgWidth;
-        let height = imgHeight;
-        if (heightRadio > radio || widthRadio > radio) {
-          if (heightRadio > widthRadio) {
-            height = (imgHeight / heightRadio) * radio;
-            width = (imgWidth / heightRadio) * radio;
+        image.src = this.images[temp];
+        image.onload = function () {
+          if (temp < len) {
+            temp++;
+            loadImg();
           }
-          // else {
-          //   height = (imgHeight / widthRadio) * radio;
-          //   width = (imgWidth / widthRadio) * radio;
-          // }
-        }
-        this.setStyle(imgNode, {
-          width: width + "px",
-          height: height + "px",
-          "vertical-align": "middle",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          margin: "auto auto",
-        });
-        imgNode.draggable = true;
-        this.$refs.post.append(imgNode);
-      },
-      getStyle(element, styleName) {
-        if (!element || !styleName) return null;
-        // styleName = camelize(styleName);
-        if (styleName === "float") {
-          styleName = "cssFloat";
-        }
-        const styleObj = element.style;
-        try {
-          const style = styleObj[styleName];
-          if (style) return style;
-          const computed = document.defaultView.getComputedStyle(element, "");
-          return computed ? computed[styleName] : "";
-        } catch (e) {
-          return styleObj[styleName];
-        }
-      },
-      setStyle(element, styleName, value) {
-        if (!element || !styleName) return;
-
-        if (styleName instanceof CSSStyleDeclaration || this.isObject(styleName)) {
-          Object.keys(styleName).forEach((prop) => {
-            this.setStyle(element, prop, styleName[prop]);
-          });
-        } else {
-          element.style[styleName] = value;
-        }
-      },
-      magnify() {
-        if (this.imgNode) {
-          let width = this.getNumberFromPx(this.imgNode.width);
-          let height = this.getNumberFromPx(this.imgNode.height);
-          this.setStyle(this.imgNode, "width", width * 1.1 + "px");
-          this.setStyle(this.imgNode, "height", height * 1.1 + "px");
-        }
-      },
-      deflate() {
-        if (this.imgNode) {
-          let width = this.getNumberFromPx(this.imgNode.width);
-          let height = this.getNumberFromPx(this.imgNode.height);
-          this.setStyle(this.imgNode, "width", width * 0.9 + "px");
-          this.setStyle(this.imgNode, "height", height * 0.9 + "px");
-        }
-      },
-      getNumberFromPx(str) {
-        if (str) {
-          if (isNaN(str)) {
-            return str.substr(0, str.length - 2);
-          } else {
-            return str;
-          }
-        }
-        return 0;
-      },
-      handLeft() {
-        this.rotateIndex--;
-        this.rotateIndex = this.rotateIndex % 4;
-        this.setRotateStyle();
-      },
-      handRight() {
-        this.rotateIndex++;
-        this.rotateIndex = this.rotateIndex % 4;
-        this.setRotateStyle();
-      },
-      setRotateStyle() {
-        this.setStyle(this.imgNode, "transform", "rotate(" + this.rotateIndex * 90 + "deg)");
-      },
-      isObject(obj) {
-        return Object.prototype.toString.call(obj) == "[object Object]";
-      },
-      getTarget(e) {
-        return e.target;
-      },
+        };
+      }
+      if (this.preload) {
+        loadImg();
+      }
     },
-  };
+    handleImgError(e) {
+      e.target.alt = "加载失败";
+    },
+    handleImgLoaded(e) {
+      if (this.imgNode && this.imgNode.parentNode) {
+        this.imgNode.parentNode.removeChild(this.imgNode);
+      }
+      let imgNode = (this.imgNode = e.target);
+      this.resetTransform();
+      this.setImgNodeStyle();
+      imgNode.onmousedown = this.handleMouseDown;
+      this.$refs.post.append(imgNode);
+    },
+    magnify() {
+      if (this.imgNode) {
+        this.transform.scale = this.transform.scale * 1.1;
+        this.setImgNodeStyle();
+      }
+    },
+    deflate() {
+      if (this.imgNode) {
+        this.transform.scale = this.transform.scale * 0.9;
+        this.setImgNodeStyle();
+      }
+    },
+    handLeft() {
+      this.transform.rotate--;
+      this.setImgNodeStyle();
+    },
+    handRight() {
+      this.transform.rotate++;
+      this.setImgNodeStyle();
+    },
+    resetStyle() {
+      this.resetTransform();
+      this.setImgNodeStyle();
+    },
+    setImgNodeStyle() {
+      let rotate = (this.transform.rotate * this.minRotate) % 360;
+      setStyle(this.imgNode, {
+        transform: `scale(${this.transform.scale})rotate(${rotate}deg)`,
+        "max-height": "100%",
+        "max-width": "100%",
+        "vertical-align": "middle",
+        "margin-top": this.transform.offsetY + "px",
+        "margin-left": this.transform.offsetX + "px",
+      });
+    },
+    resetTransform() {
+      this.transform = JSON.parse(JSON.stringify(defaultTransform));
+    },
+  },
+  destroyed() {
+    this.uninstallEvent();
+    if (this.appendToBody && this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
+  },
+};
 </script>
 <style scoped>
-  * {
-    box-sizing: border-box;
-  }
-  .pictureViewer {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 1000;
-  }
-  .mask {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: #000;
-    opacity: 0.75;
-    z-index: inherit;
-  }
-  .container {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    z-index: 10001;
-    padding-bottom: 50px;
-  }
-  .side {
-    position: fixed;
-    z-index: 10002;
-    bottom: 0;
-    height: 35px;
-    top: 0;
-    margin: auto 0;
-    cursor: pointer;
-  }
-  .side.left {
-    left: 50px;
-  }
-  .side.right {
-    right: 50px;
-  }
-  .bottom {
-    position: fixed;
-    left: 0;
-    right: 0;
-    height: 50px;
-    bottom: 0;
-    z-index: 10001;
-    text-align: center;
-  }
-  button {
-    vertical-align: middle;
-    cursor: pointer;
-  }
-  .imagePost {
-    overflow: hidden;
-    height: 100%;
-    position: relative;
-  }
-  .imagePost img {
-    position: absolute;
-    /* left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    margin: auto auto; */
-  }
-  .close {
-    position: fixed;
-    top: 0;
-    right: 0;
-    background-color: #fff;
-    z-index: 10001;
-    cursor: pointer;
-  }
+.pictureViewer {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-sizing: border-box;
+}
+.pictureViewer-verticlehelp {
+  height: 100%;
+  width: 0;
+  line-height: initial;
+  vertical-align: middle;
+  display: inline-block;
+}
+.pictureViewer-mask {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #000;
+  opacity: 0.5;
+}
+.pictureViewer-container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+  text-align: center;
+}
+.pictureViewer-side {
+  position: fixed;
+  z-index: 10002;
+  bottom: 0;
+  height: 35px;
+  top: 0;
+  margin: auto 0;
+  cursor: pointer;
+}
+.pictureViewer-side.left {
+  left: 50px;
+}
+.pictureViewer-side.right {
+  right: 50px;
+}
+.pictureViewer-bottom {
+  position: fixed;
+  left: 0;
+  right: 0;
+  height: 50px;
+  bottom: 0;
+  z-index: 10001;
+  text-align: center;
+}
+button {
+  vertical-align: middle;
+  cursor: pointer;
+}
+.pictureViewer-close {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 10001;
+  cursor: pointer;
+}
 </style>
